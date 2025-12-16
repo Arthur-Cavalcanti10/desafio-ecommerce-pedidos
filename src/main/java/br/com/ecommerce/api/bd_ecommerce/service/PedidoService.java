d08cff97ea6d5a627a6a07a3eb959b764c9709e5
Arthur Cavalcanti <cavalcantiarthur2007@gmail.com>
2025-12-16 13:29:26 -0300

    commit -m "alteração em algumas logicas"

--- src/main/java/br/com/ecommerce/api/bd_ecommerce/service/PedidoService.java
+++ src/main/java/br/com/ecommerce/api/bd_ecommerce/service/PedidoService.java
@@ -3,6 +3,7 @@ package br.com.ecommerce.api.bd_ecommerce.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,19 @@ import org.springframework.stereotype.Service;
 import br.com.ecommerce.api.bd_ecommerce.model.Pedido;
 import br.com.ecommerce.api.bd_ecommerce.model.Produto;
 import br.com.ecommerce.api.bd_ecommerce.repository.PedidoRepository;
+import br.com.ecommerce.api.bd_ecommerce.repository.ProdutoRepository;
 
 @Service
 public class PedidoService {
   @Autowired
   private PedidoRepository pedidoRepository;
-
+  private ProdutoRepository produtoRepository;
 
   public Pedido criarPedido(Pedido pedido) {
   double valorTotal=0;
+  List<Long> produtoIds = pedido.getProdutos().stream().map(Produto::getId).collect(Collectors.toList());//extraio os ids que foram passados
+  List<Produto> produtos = produtoRepository.findAllById(produtoIds); // caço os produtos de cada id da lista
+
   for(Produto p : pedido.getProdutos()){
     valorTotal+=p.getPreco(); 
   }
@@ -26,14 +31,17 @@ public class PedidoService {
   if (valorTotal>200){
     valorTotal=valorTotal*0.9;
     pedido.setValor_total(valorTotal);
-
+    pedido.setProdutos(produtos);
     return pedidoRepository.save(pedido);
   }
 
+  pedido.setProdutos(produtos); //seto os produtos que eu achei da lista no pedido
   pedido.setValor_total(valorTotal);
    
   return pedidoRepository.save(pedido);
   }
+
+
 
   public List<Pedido> listarPedidos() {
     return pedidoRepository.findAll();
